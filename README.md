15d5c016c94512c934fc4da6839040c2d2fefbcd
Cytezera <ezrachin05@gmail.com>
2025-01-26 01:16:16 +0800

    Finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# 21 Number Game
+
+The computer will win 100% of the time if it's given the first turn or the player makes a mistake 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 def playerTurn(currentValue):
     while True: 
         userInput = int(input("From a range from 1 - 3, how much you would like to put? "))
@@ -6,28 +7,56 @@ def playerTurn(currentValue):
             return (currentValue + userInput)  
         print ("Invalid input. Please try again" ) 
 def computerTurn(currentValue):
-    compInput =  
+    if (currentValue % 4 > 0) : 
+        computerInput = 4 - (currentValue % 4 ) 
+    else : 
+        computerInput = random.choice([1,2,3])
+    print (f"Computer's chocie: {computerInput}")
+    return (currentValue + computerInput)
+
 def determineMove(currentValue,turn):
-    if turn == player: 
-        plyaerTurn(currentValue)
+    if turn == "player": 
+        currentValue = playerTurn(currentValue)
     else:
-        computerTurn(currentValue)
-    return currentValue
+        currentValue = computerTurn(currentValue)
+    
+    return currentValue if currentValue <22 else 21 
 def chooseTurn():
     while True:
-        userInput = input("Would you like to be first or second") 
-        if userInput == "first" or input == "second":
+        userInput = input("Would you like to be first or second \n") 
+        if userInput == "first" or userInput == "second":
             return "player" if userInput == "first" else "comp" 
         print ("Invalid input, please try again " ) 
+def displayGame(turn,currentValue):
+    print (f"Currenet Value: {currentValue}")
+    if turn == "player":
+        print ("Player's turn" )
+    else : 
+        print ("Computer's turn")
+def changeTurns(turn):
+    if turn == "player":
+        turn = "comp"
+    else:
+        turn = "player"
+    return turn 
+def showResult(turn):
+    if turn == "player":
+        print ("Comoputer wins!") 
+    else : 
+        print ("Player wins!") 
+
 def playGame():
     currentValue = 1 
     turn = chooseTurn()
     while currentValue < 21:
+        displayGame(turn, currentValue)      
         currentValue = determineMove(currentValue,turn)
-          
+        if (currentValue == 21): 
+            showResult(turn)
+        turn = changeTurns(turn)
 def menu():
-	while True: 
-        choice = int(input("What would you like to do? \n1.Play\n2.Exit"))
+    while True:
+        choice = int(input("What would you like to do ? \n1.Play\n2.Exit"))
         match choice:
             case 1:
                 playGame()
